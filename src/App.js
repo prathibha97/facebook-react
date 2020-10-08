b57bff19a534476dbeb35575e319dc330bc9f0e0
Prathibha Ratnayake <prsthibha@gmail.com>
2020-10-08 13:30:53 +0530

    login component created

--- src/App.js
+++ src/App.js
@@ -2,19 +2,26 @@ import React from "react";
 import "./App.css";
 import Feed from "./components/Feed";
 import Header from "./components/Header";
+import Login from "./components/Login";
 import Sidebar from "./components/Sidebar";
 import Widget from "./components/Widget";
 
 function App() {
+  const user = null;
   return (
     <div className='app'>
-      <Header />
-      <div className='app__body'>
-        <Sidebar/>
-        <Feed/>
-        {/* widget */}
-        <Widget/>
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <>
+          <Header />
+          <div className='app__body'>
+            <Sidebar />
+            <Feed />
+            <Widget />
+          </div>
+        </>
+      )}
     </div>
   );
 }
