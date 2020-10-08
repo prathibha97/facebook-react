10abbc58c52f369ef709344c679728cbf5cc13a9
Prathibha Ratnayake <prsthibha@gmail.com>
2020-10-08 11:18:54 +0530

    story styled

--- src/components/StoryReel.js
+++ src/components/StoryReel.js
@@ -1,12 +1,31 @@
-import React from 'react'
-import '../css/StoryReel.css'
-import Story from './Story'
+import React from "react";
+import "../css/StoryReel.css";
+import Story from "./Story";
 function StoryReel() {
-    return (
-        <div className='storyReel'>
-            <Story/>
-        </div>
-    )
+  return (
+    <div className='storyReel'>
+      <Story
+        image='https://scontent.fcmb4-1.fna.fbcdn.net/v/t1.0-9/17190390_1284898914925216_4438241606510305508_n.jpg?_nc_cat=101&_nc_sid=e3f864&_nc_ohc=W6ICCq0VzBsAX9ONDOT&_nc_ht=scontent.fcmb4-1.fna&oh=8c1b4ab28404fdcbad94792c892b6b1f&oe=5FA26C34'
+        profileSrc='https://scontent.fcmb4-1.fna.fbcdn.net/v/t1.0-9/14681867_1159593897455719_3755886871151574095_n.jpg?_nc_cat=107&_nc_sid=09cbfe&_nc_ohc=LpDNkWSk2H0AX8NAjWy&_nc_ht=scontent.fcmb4-1.fna&oh=8bf9d4f17bf76706658b9b22d3b6b2f7&oe=5FA5631B'
+        title='Prathibha'
+      />
+      <Story
+        image='https://scontent.fcmb4-1.fna.fbcdn.net/v/t1.0-9/120611302_2953267158230264_2410385716684304772_n.jpg?_nc_cat=106&_nc_sid=e3f864&_nc_ohc=27wOa4r-kNwAX80mw2L&_nc_ht=scontent.fcmb4-1.fna&oh=b0e443d9b37b97c80c76452792a45c5c&oe=5FA55A40'
+        profileSrc='https://scontent.fcmb4-1.fna.fbcdn.net/v/t1.0-9/118395623_2921634498060197_8866140339331709337_n.jpg?_nc_cat=103&_nc_sid=09cbfe&_nc_ohc=eP7c9hQ8VH8AX9EnUJf&_nc_ht=scontent.fcmb4-1.fna&oh=a7b8affe705a938aac135602e8498c35&oe=5FA611D7'
+        title='Pasindu'
+      />
+      <Story
+        image='https://scontent.fcmb4-1.fna.fbcdn.net/v/t1.0-0/p640x640/70255428_505312316954166_3393102697317007360_o.jpg?_nc_cat=102&_nc_sid=e3f864&_nc_ohc=7B2Mx1_KtpwAX_kp5Cl&_nc_ht=scontent.fcmb4-1.fna&tp=6&oh=d2747a1f0fab3f3ff3b8ab68c9527d6b&oe=5FA2F99A'
+        profileSrc='https://scontent.fcmb4-1.fna.fbcdn.net/v/t1.0-9/106454213_722155978603131_5565567385245882739_o.jpg?_nc_cat=110&_nc_sid=09cbfe&_nc_ohc=UyuVfyiqLs8AX9Ce_c3&_nc_ht=scontent.fcmb4-1.fna&oh=89bcc351ab21b7b13e69361e4de4594a&oe=5FA5E034'
+        title='Pasan'
+      />
+      <Story
+        image='https://scontent.fcmb4-1.fna.fbcdn.net/v/t1.0-9/92670318_2986498598078893_8618470230160572416_n.jpg?_nc_cat=105&_nc_sid=e3f864&_nc_ohc=5Uho4LvWnLEAX_fmlUt&_nc_ht=scontent.fcmb4-1.fna&oh=4f990391fca93f7bcbd03f9995f1a15b&oe=5FA44389'
+        profileSrc='https://scontent.fcmb4-1.fna.fbcdn.net/v/t1.0-9/87144908_2883389405056480_7719796199473020928_n.jpg?_nc_cat=103&_nc_sid=09cbfe&_nc_ohc=fbUzRz5ovUAAX_NO_Rx&_nc_ht=scontent.fcmb4-1.fna&oh=ca2d4191fbe0601e0e25493c6c2baf13&oe=5FA610F5'
+        title='Althaff'
+      />
+    </div>
+  );
 }
 
-export default StoryReel
+export default StoryReel;
