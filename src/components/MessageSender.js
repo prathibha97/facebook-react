a64efe6ca1c04e2fed08f4eea5520057e2cb739d
Prathibha Ratnayake <prsthibha@gmail.com>
2020-10-08 12:09:54 +0530

    messageSender styled

--- src/components/MessageSender.js
+++ src/components/MessageSender.js
@@ -1,11 +1,43 @@
-import React from 'react'
-import '../css/MessageSender.css'
+import { Avatar } from "@material-ui/core";
+import { InsertEmoticon, PhotoLibrary, Videocam } from "@material-ui/icons";
+import React from "react";
+import "../css/MessageSender.css";
 function MessageSender() {
-    return (
-        <div className='messageSender'>
-            
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className='messageSender'>
+      <div className='messageSender__top'>
+        <Avatar />
+        <form>
+          <input
+            className='messageSender__input'
+            type='text'
+            placeholder="What's on your mind?"
+          />
+          <input type='text' placeholder='image URL (Optional)' />
+          <button type='submit' onClick={handleSubmit}>
+            Submit
+          </button>
+        </form>
+      </div>
+      <div className='messageSender__Bottom'>
+        <div className='messageSender__option'>
+          <Videocam style={{ color: "red" }} />
+          <h3>Live Video</h3>
         </div>
-    )
+        <div className='messageSender__option'>
+          <PhotoLibrary style={{ color: "green" }} />
+          <h3>Photo/Video</h3>
+        </div>
+        <div className='messageSender__option'>
+          <InsertEmoticon style={{ color: "orange" }} />
+          <h3>Feeling/Activity</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default MessageSender
+export default MessageSender;
