a4354e110ed181977bff6c113bce6d9ad9f3bb67
Prathibha Ratnayake <prsthibha@gmail.com>
2020-10-08 09:19:11 +0530

    header component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="app">
-      
+      {/* header */}
+      <Header/>
+      {/* app body */}
+          {/* sidebar */}
+          {/* feed */}
+          {/* widget */}
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBMe3Z81-yh9FQ3T_QuqS3zE2GMUe3hseg",
+  authDomain: "facebook-react-27e26.firebaseapp.com",
+  databaseURL: "https://facebook-react-27e26.firebaseio.com",
+  projectId: "facebook-react-27e26",
+  storageBucket: "facebook-react-27e26.appspot.com",
+  messagingSenderId: "499584262356",
+  appId: "1:499584262356:web:6f3e0c869a47ccf7a35b41",
+  measurementId: "G-4ZDQZWSKJN",
+};
