c281ecffc41252a0fb7d40d86d39c0913c69446d
Prathibha Ratnayake <prsthibha@gmail.com>
2020-10-08 12:18:05 +0530

    post component created

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import '../css/Feed.css'
 import MessageSender from './MessageSender'
+import Post from './Post'
 import StoryReel from './StoryReel'
 function Feed() {
     return (
         <div className='feed'>
             <StoryReel/>
-            {/* message sender */}
             <MessageSender/>
+            <Post/>
         </div>
     )
 }
