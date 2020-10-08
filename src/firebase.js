8483982f07605858f5bc8922d259dd95601fa462
Prathibha Ratnayake <prsthibha@gmail.com>
2020-10-08 13:50:52 +0530

    firebase config added

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,4 @@
+import firebase from "firebase";
 const firebaseConfig = {
   apiKey: "AIzaSyBMe3Z81-yh9FQ3T_QuqS3zE2GMUe3hseg",
   authDomain: "facebook-react-27e26.firebaseapp.com",
@@ -8,3 +9,14 @@ const firebaseConfig = {
   appId: "1:499584262356:web:6f3e0c869a47ccf7a35b41",
   measurementId: "G-4ZDQZWSKJN",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+const auth = firebase.auth();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
