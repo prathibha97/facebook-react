9993591038acec1dde82ec1ed0e103a1966282eb
Prathibha Ratnayake <prsthibha@gmail.com>
2020-10-08 12:54:44 +0530

    post styled

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,16 +1,22 @@
-import React from 'react'
-import '../css/Feed.css'
-import MessageSender from './MessageSender'
-import Post from './Post'
-import StoryReel from './StoryReel'
+import React from "react";
+import "../css/Feed.css";
+import MessageSender from "./MessageSender";
+import Post from "./Post";
+import StoryReel from "./StoryReel";
 function Feed() {
-    return (
-        <div className='feed'>
-            <StoryReel/>
-            <MessageSender/>
-            <Post/>
-        </div>
-    )
+  return (
+    <div className='feed'>
+      <StoryReel />
+      <MessageSender />
+      <Post
+        profilePic='https://scontent.fcmb4-1.fna.fbcdn.net/v/t1.0-9/14681867_1159593897455719_3755886871151574095_n.jpg?_nc_cat=107&_nc_sid=09cbfe&_nc_ohc=LpDNkWSk2H0AX8NAjWy&_nc_ht=scontent.fcmb4-1.fna&oh=8bf9d4f17bf76706658b9b22d3b6b2f7&oe=5FA5631B'
+        message='test post'
+        timestamp='demo timestamp'
+        username='Prathibha'
+        image='https://scontent.fcmb4-1.fna.fbcdn.net/v/t1.0-9/17190390_1284898914925216_4438241606510305508_n.jpg?_nc_cat=101&_nc_sid=e3f864&_nc_ohc=W6ICCq0VzBsAX9ONDOT&_nc_ht=scontent.fcmb4-1.fna&oh=8c1b4ab28404fdcbad94792c892b6b1f&oe=5FA26C34'
+      />
+    </div>
+  );
 }
 
-export default Feed
+export default Feed;
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,51 @@
-import React from 'react'
-import '../css/Post.css'
-function Post() {
-    return (
-        <div className='post'>
-            
+import { Avatar } from "@material-ui/core";
+import {
+  AccountCircle,
+  ChatBubbleOutline,
+  ExpandMoreOutlined,
+  NearMe,
+  ThumbUp,
+} from "@material-ui/icons";
+import React from "react";
+import "../css/Post.css";
+function Post({ profilePic, image, username, message, timestamp }) {
+  return (
+    <div className='post'>
+      <div className='post__top'>
+        <Avatar src={profilePic} className='post__avatar' />
+        <div className='post__topInfo'>
+          <h3>{username}</h3>
+          {/* <p>{new Date(timestamp?.toDate()).toUTCString()}</p> */}
+          <p>timestamp goes here</p>
         </div>
-    )
+      </div>
+
+      <div className='post__bottom'>
+        <p>{message}</p>
+      </div>
+      <div className='post__image'>
+        <img src={image} alt='' />
+      </div>
+      <div className='post__options'>
+        <div className='post__option'>
+          <ThumbUp />
+          <p>Like</p>
+        </div>
+        <div className='post__option'>
+          <ChatBubbleOutline />
+          <p>Comment</p>
+        </div>
+        <div className='post__option'>
+          <NearMe />
+          <p>Share</p>
+        </div>
+        <div className='post__option'>
+          <AccountCircle />
+          <ExpandMoreOutlined />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Post
+export default Post;
